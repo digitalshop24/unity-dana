25e075b0e2fa3d6189071348be9c117ccf72040d
Savushkin Zhenya <savik94mail@gmail.com>
2016-06-09 20:53:02 +0300

    added logout

--- src/dashboard/header/controller.js
+++ src/dashboard/header/controller.js
@@ -1,15 +1,22 @@
 'use strict';
 
 export default class HeaderCtrl {
-    constructor(login, cardStorage, $state, session) {
+    constructor(login, cardStorage, $state, session, auth) {
         this.loginService = login;
         this.cardStorage = cardStorage;
         this.session = session;
+        this.auth = auth;
         this.$state = $state;
     }
 
     openLoginPopup() {
         this.loginService.open();
+    }
+
+    logout() {
+        this.auth.logout().then(()=> {
+            this.$state.go('dashboard.main');
+        });
     }
 
     search() {
