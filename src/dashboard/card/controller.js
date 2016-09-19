fb9712b2e173ac034859b35f516dfbdc46db5719
gitwhatislove <whatislove0505@gmail.com>
2016-09-19 14:53:42 +0300

    'add'

--- src/dashboard/card/controller.js
+++ src/dashboard/card/controller.js
@@ -20,7 +20,7 @@ export default class CardCtrl {
         this.requisits = requisits;
         this.req_content = this.sce.trustAsHtml(this.requisits.content);
         this.orderNumber = '';
-        this.error1 = '';
+        this.errorText = '';
         
     }
 
@@ -60,7 +60,6 @@ export default class CardCtrl {
                 this.orderNumber = res.id;
                 // this.state.go('dashboard.thx');
             }).catch((err) => {
-                console.log(err.error);
                 this.errorText = err.data.error || "Произошла ошибка регистрации";
             });
 
--- src/dashboard/month-new/controller.js
+++ src/dashboard/month-new/controller.js
@@ -3,5 +3,6 @@
 export default class MonthNewCtrl {
     constructor(newGoods) {
         this.newGoods = newGoods;
+        console.log(this.newGoods);
     }
 }
