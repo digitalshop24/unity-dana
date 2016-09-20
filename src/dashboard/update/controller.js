980a57badb01d29d20fd94e58281971285e08afd
gitwhatislove <whatislove0505@gmail.com>
2016-09-20 14:35:55 +0300

    'add'

--- src/dashboard/update/controller.js
+++ src/dashboard/update/controller.js
@@ -2,9 +2,17 @@
 
 
 export default class UpdateCtrl {
-    constructor(auth) {
+    constructor(auth, session) {
     	this.auth = auth;
         this.text = '';
+        this.session = session;
+        console.log(this.session.user);
+        this.isAuthenticated = this.session.isAuthenticated;
+        if (this.session.user.role == 'Admin') {
+            this.role = true;
+        }
+        else this.role = false;
+        
     }
 
     update() {
